fd4b5c92c1fe68cc64fece138d521dfdab59b0e5
Yuvraj Soni <demonsroar2172@gmail.com>
2023-12-08 16:59:37 +0530

    charts

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,34 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import {Form} from "./PitchForm";
+import { Form } from "./PitchForm";
 
 const Home = () => {
   const [check, setCheck] = useState(false);
-  function handleCheck() {}
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `/pitch`;
+    navigate(path);
+  };
   return (
-    <div className="home-page  ">
-      <button
-        onClick={() => setCheck(!check)}
-        className=" absolute left-20 top-20  px-4 py-2 font-semibold text-sm bg-blue-500 text-white rounded-md shadow-sm hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 ease-in-out delay-150 duration-300  "
-      >
-        Check your pitch
-      </button>
-      {check && <Form />}
+    <div className="home-page">
+      <div className="flex gap-5 py-2 ">
+        <div  >
+          <button
+            onClick={() => setCheck(!check)}
+            className="  mx-20 px-4 py-2 font-semibold text-sm bg-blue-500 text-white rounded-md shadow-sm hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 ease-in-out delay-150 duration-300  "
+          >
+            Check your pitch
+          </button>
+          {check && <Form />}
+          <button
+            onClick={routeChange}
+            className="  px-4 py-2 font-semibold text-sm bg-blue-500 text-white rounded-md shadow-sm hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 ease-in-out delay-150 duration-300  "
+          >
+            Predict Analysis
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/PitchForm.jsx
+++ src/components/PitchForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const arr = [
   "agriculture",
@@ -19,14 +20,26 @@ const arr = [
 ];
 
 export function Form() {
-  const [show, setShow] = useState(true);
-  const [name, setName] = useState("");
+  const [userName, setUserName] = useState("");
   const [product, setProduct] = useState("");
   const [investment, setInvestment] = useState("");
   const [equity, setEquity] = useState("");
-  const [industry] = useState("");
+  const [industry, setIndustry] = useState("");
+  const [summary, setSummary] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `/pitch`;
+    navigate(path);
+    console.log(path);
+  };
   return (
-    <form className=" p-5 rounded-lg w-2/3 bg-white absolute inset-x-60 inset-y-50 my-20 ">
+    <form
+      className=" p-5 rounded-lg w-2/3 bg-white absolute inset-x-60 inset-y-50 my-20"
+      onSubmit={handleSubmit}
+    >
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">
@@ -36,7 +49,7 @@ export function Form() {
           <div className="mt-5 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
               <label
-                htmlFor="first-name"
+                htmlFor="userName"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
                 Name
@@ -44,8 +57,12 @@ export function Form() {
               <div className="mt-2">
                 <input
                   type="text"
-                  name="first-name"
+                  name="userName"
                   id="first-name"
+                  value={userName}
+                  onChange={(e) => {
+                    setUserName(e.target.value);
+                  }}
                   autoComplete="given-name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -62,7 +79,9 @@ export function Form() {
               <div className="mt-2">
                 <input
                   type="text"
-                  name="product-name"
+                  name="product"
+                  value={product}
+                  onChange={(e) => setProduct(e.target.value)}
                   id="product-name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -80,6 +99,8 @@ export function Form() {
                   type="text"
                   name="investment"
                   id="investment"
+                  value={investment}
+                  onChange={(e) => setInvestment(e.target.value)}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -93,6 +114,8 @@ export function Form() {
               </label>
               <div className="mt-2">
                 <input
+                  value={equity}
+                  onChange={(e) => setEquity(e.target.value)}
                   type="text"
                   name="equity"
                   id="equity"
@@ -114,6 +137,8 @@ export function Form() {
                   cols={40}
                   id="summary"
                   name="summary"
+                  value={summary}
+                  onChange={(e) => setSummary(e.target.value)}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -130,6 +155,8 @@ export function Form() {
                 <input
                   id="industry"
                   name="industry"
+                  value={industry}
+                  onChange={(e) => setIndustry(e.target.value)}
                   autoComplete="industry-name"
                   className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                 ></input>
@@ -149,9 +176,11 @@ export function Form() {
         <button
           type="submit"
           className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          
         >
           Save
         </button>
+        
       </div>
     </form>
   );
